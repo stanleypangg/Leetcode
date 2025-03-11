e01a02e6245eb37ada5b8d7b67ff338a6a6e139c
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-03-10 23:11:48 -0400

    Time: 3 ms (83.1%), Space: 17.7 MB (69.79%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_check = [set() for i in range(9)]
+        col_check = [set() for i in range(9)]
+        box_check = [set() for i in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                curr = board[i][j]
+                if curr == '.':
+                    continue
+
+                if curr in row_check[i]:
+                    return False
+                row_check[i].add(curr)
+
+                if curr in col_check[j]:
+                    return False
+                col_check[j].add(curr)
+                
+                if curr in box_check[i // 3 * 3 + j // 3]:
+                    return False
+                box_check[i // 3 * 3 + j // 3].add(curr)
+        
+        return True
