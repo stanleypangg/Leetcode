32888e1263ebe8fb332cfbad76ef2c84488b06db
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-03-16 04:07:33 -0400

    Time: 36 ms (94.1%), Space: 52.3 MB (40.64%) - LeetHub

---
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const filtered = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) filtered.push(arr[i]);
+    }
+    return filtered;
+};
