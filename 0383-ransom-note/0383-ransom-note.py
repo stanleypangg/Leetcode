402cf16077541900c44c7c32df15aab4d5019821
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-03-16 03:18:51 -0400

    Time: 23 ms (49.79%), Space: 18 MB (54.46%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,12 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        hashmap = defaultdict(int)
-        for c in ransomNote:
-            hashmap[c] += 1
-        
+        mag_hash = {}
         for c in magazine:
-            hashmap[c] -= 1
+            mag_hash[c] = mag_hash.get(c, 0) + 1
         
-        return all(count <= 0 for count in hashmap.values())
+        for c in ransomNote:
+            if c not in mag_hash or mag_hash[c] <= 0:
+                return False
+            mag_hash[c] -= 1
+        
+        return True
