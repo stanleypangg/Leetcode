e83cd7f63f097b94a9007ded0cfef8975e5ad002
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-03-16 04:10:47 -0400

    Time: 54 ms (38.67%), Space: 54.6 MB (24.83%) - LeetHub

---
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    if (nums.length === 0) return init;
+    for (const elem of nums) init = fn(init, elem);
+    return init;
+};
