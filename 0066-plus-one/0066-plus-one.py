31a26aa5bf718bed5254a663426d092211f12fcf
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-03-25 23:58:53 -0400

    Time: 0 ms (100%), Space: 17.6 MB (80.24%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                carry = 0
+                break
+        
+        if carry == 1:
+            digits.insert(0, 1)
+
+        return digits
