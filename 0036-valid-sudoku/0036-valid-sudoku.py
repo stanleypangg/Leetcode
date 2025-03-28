620dda9f0b1e87e9255b74858209fded9598d125
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-03-28 03:26:14 -0400

    Time: 6 ms (44.04%), Space: 17.7 MB (69.41%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,7 +2,7 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         row_check = [set() for _ in range(9)]
         col_check = [set() for _ in range(9)]
-        box_check = [set() for _ in range(9)]
+        box_check = [[set() for _ in range(3)] for _ in range(3)]
 
         for i in range(9):
             for j in range(9):
@@ -18,8 +18,8 @@ class Solution:
                     return False
                 col_check[j].add(curr)
 
-                if curr in box_check[i // 3 * 3 + j // 3]:
+                if curr in box_check[i // 3][j // 3]:
                     return False
-                box_check[i // 3 * 3 + j // 3].add(curr)
+                box_check[i // 3][j // 3].add(curr)
         
         return True
