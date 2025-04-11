2422dff2fbabd5f64c705fba0c8536f47e9b0850
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-04-11 04:20:58 -0400

    Time: 0 ms (100%), Space: 18.1 MB (26.42%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,20 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
+        ret = []
         st = []
 
         def bt(l, r):
             if l == 0 and r == 0:
-                res.append(''.join(st))
+                ret.append(''.join(st))
             if l > 0:
                 st.append('(')
                 bt(l - 1, r)
                 st.pop()
             if r > l:
                 st.append(')')
-                bt(l, r - 1)
+                bt(l, r -1 )
                 st.pop()
         
         bt(n, n)
 
-        return res
+        return ret
+            
