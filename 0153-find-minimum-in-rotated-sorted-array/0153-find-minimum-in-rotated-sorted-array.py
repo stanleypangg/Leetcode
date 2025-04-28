8e253a997fb56894b9f9d3909dfc8863e6b3471c
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-04-28 00:12:47 -0400

    Time: 0 ms (100%), Space: 18.2 MB (9.52%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -10,9 +10,9 @@ class Solution:
 
             mid = (l + r) // 2
             res = min(res, nums[mid])
-            if nums[mid] >= nums[l]:
-                l = mid + 1
-            else:
+            if nums[mid] <= nums[r]:
                 r = mid - 1
-
+            else:
+                l = mid + 1
+        
         return res
