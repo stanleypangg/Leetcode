66d6c60e5880c736af5847e97ca91efdc67affa0
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-05-05 19:05:41 -0400

    Time: 15 ms (85.86%), Space: 17.9 MB (63.09%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -12,22 +12,17 @@ class Solution:
         for i in range(len(s1)):
             window[s2[i]] = window.get(s2[i], 0) + 1
         
-        l, r = 0, len(s1)
+        l = 0
         # sliding window
-        while r <= len(s2):
+        for r in range(len(s1), len(s2)):
             if window == s1_count:
                 return True
-
-            if r == len(s2):
-                break
 
             window[s2[l]] -= 1
             if window[s2[l]] == 0:
                 del window[s2[l]]
             
             window[s2[r]] = window.get(s2[r], 0) + 1
-            
             l += 1
-            r += 1
 
-        return False
+        return window == s1_count
