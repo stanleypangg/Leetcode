e7f7673bb3c431da8b4eb5ce37135cb5aaa443a7
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-05-21 01:43:05 -0400

    Time: 47 ms (9.76%), Space: 30.1 MB (7.46%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -14,24 +14,23 @@ class Solution:
             q.append((r, c))
             is_surrounded = True
             while q:
-                for _ in range(len(q)):
-                    r, c = q.popleft()
-                    if r == 0 or r == m - 1 or c == 0 or c == n - 1:
-                        is_surrounded = False
-                    for dr, dc in self.directions:
-                        new_r, new_c = r + dr, c + dc
-                        if (0 <= new_r < m and 0 <= new_c < n 
-                                and board[new_r][new_c] == "O" and (new_r, new_c) not in self.visited):
+                r, c = q.popleft()
+                if r == 0 or r == m - 1 or c == 0 or c == n - 1:
+                    is_surrounded = False
+
+                for dr, dc in self.directions:
+                    new_r, new_c = r + dr, c + dc
+                    if 0 <= new_r < m and 0 <= new_c < n and (new_r, new_c) not in self.visited:
+                        self.visited.add((new_r, new_c))
+                        if board[new_r][new_c] == "O":
                             q.append((new_r, new_c))
                             region.add((new_r, new_c))
-                        self.visited.add((new_r, new_c))
+
             return region, is_surrounded
 
-        for r in range(m):
-            for c in range(n):
-                if (r, c) in self.visited:
-                    continue
-                if board[r][c] == 'O':
+        for r in range(1, m - 1):
+            for c in range(1, n - 1):
+                if board[r][c] == 'O' and (r, c) not in self.visited:
                     region, is_surrounded = find_region(r, c)
                     if is_surrounded:
                         for region_r, region_c in region:
