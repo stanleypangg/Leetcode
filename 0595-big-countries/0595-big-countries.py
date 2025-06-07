12ea827e748ccc167daaf0cdeb27b84c41f9ecbd
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-06-07 13:36:22 -0400

    Time: 263 ms (63.51%), Space: 69.3 MB (12.5%) - LeetHub

--- 0595-big-countries/0595-big-countries.py
+++ 0595-big-countries/0595-big-countries.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    return world[(world['area'] >= 3000000) | (world['population'] >= 25000000)][['name', 'population', 'area']]
+    cols = ['name', 'population', 'area']
+    df = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+    return df[cols]
