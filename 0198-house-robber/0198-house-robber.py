c61e3af65812977b844dae702d0cf80714f020a2
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-07-28 20:15:10 -0400

    Time: 0 ms (100%), Space: 17.7 MB (54.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1 = rob2 = 0
+        # Yes you can leave some in between
+        one = two = 0
 
         for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-
-        return rob2
+            subproblem = max(one, n + two)
+            one, two = subproblem, one
+        
+        return one
