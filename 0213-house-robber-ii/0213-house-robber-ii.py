105e99039d26771f9500559109d8ee7b5a2b0ed2
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-08-10 02:22:47 -0400

    Time: 0 ms (100%), Space: 17.9 MB (38.93%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -2,20 +2,11 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         # intuition for circular array?
         # two pass?
-        if len(nums) == 1:
-            return nums[0]
-            
-        one = two = 0
-        
-        for i in range(len(nums) - 1):
-            sub = max(one, nums[i] + two)
-            one, two = sub, one
-        
-        res = one
+        def house_rob(nums):
+            one = two = 0
+            for n in nums:
+                sub = max(one, two + n)
+                one, two = sub, one
+            return one
 
-        one = two = 0
-        for i in range(1, len(nums)):
-            sub = max(one, nums[i] + two)
-            one, two = sub, one
-        
-        return max(res, one)
+        return max(house_rob(nums[:-1]), house_rob(nums[1:]), nums[0])
