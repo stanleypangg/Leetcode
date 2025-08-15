8d14edeab62bc525dc3707166b03b419af86ecaf
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-08-15 11:32:08 -0400

    Time: 735 ms (69.76%), Space: 18 MB (97.3%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,12 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n, m = len(coins), amount
+
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, m + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
+        
+        return dp[-1] if dp[-1] != amount + 1 else -1
