1f49b509ba74305852806e41f8807dbcab7f4d24
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-08-30 02:16:01 -0400

    Time: 48 ms (60.75%), Space: 22.3 MB (97.58%) - LeetHub

---
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.py
@@ -0,0 +1,36 @@
+class Solution:
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        # commands: sequence of moves it needs to execute
+        # 3 types of "moves":
+        # -2 -> turn left 90 (counter-clockwise)
+        # =1 -> turn right 90 (clockwise)
+        # 1 <= k <= 9: move forward k units, one unit at a time
+
+        # obstacles[i] = (xi, yi)
+
+        # return: max squared euclidean distance sqrt((x1 - x0)^2 + (y1 - y0)^2) at any point
+        res = 0
+        x = y = 0
+
+        # N = 0, E = 1, S = 2, W = 3
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        facing = 0 # index 0 = North
+
+        obstacles = set(tuple(obstacle) for obstacle in obstacles)
+
+        for command in commands:
+            if command == -2:
+                facing = facing - 1 if facing > 0 else 3
+            elif command == -1:
+                facing = (facing + 1) % 4
+            else:
+                # just loop k tiems?
+                dx, dy = directions[facing]
+                for _ in range(command):
+                    new_x, new_y = x + dx, y + dy
+                    if (new_x, new_y) in obstacles:
+                        break
+                    x, y = new_x, new_y
+                    res = max(res, (x ** 2 + y ** 2))
+        
+        return res
