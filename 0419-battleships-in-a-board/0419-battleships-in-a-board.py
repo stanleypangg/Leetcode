9fe9c20ccd2060a1521678e6c021750098cf2c9e
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-09-19 02:41:34 -0400

    Time: 0 ms (100%), Space: 19.8 MB (97.4%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -2,19 +2,14 @@ class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
         total = 0
         m, n = len(board), len(board[0])
-
-        # dfs right and down upon first appearance
-        def dfs(r, c, dr, dc):
-            if 0 <= r < m and 0 <= c < n and board[r][c] == 'X':
-                board[r][c] = '.'
-                dfs(r + dr, c + dc, dr, dc)
         
         for r in range(m):
             for c in range(n):
                 if board[r][c] == 'X':
-                    dfs(r, c, 1, 0)
-                    board[r][c] = 'X'
-                    dfs(r, c, 0, 1)
+                    # we only ever explore right and down, if we see an 'X' up or left, it's already been explored
+                    if (r > 0 and board[r - 1][c] == 'X') or (c > 0 and board[r][c - 1] == 'X'):
+                        continue
+                    # only count the top left of the battleship 
                     total += 1
         
         return total
