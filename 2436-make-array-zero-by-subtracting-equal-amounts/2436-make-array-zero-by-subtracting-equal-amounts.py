12dbce04d1916f5e18f938c4aa5d9b5d8a006902
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-09-20 03:09:19 -0400

    Time: 0 ms (100%), Space: 17.8 MB (31.94%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.py
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,7 +1,3 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        num_set = set(nums)
-        length = len(num_set)
-        if 0 in num_set:
-            length -= 1
-        return length
+        return len(set(nums) - {0})
