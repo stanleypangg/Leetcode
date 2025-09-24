6b23e291046a0f843c34206fd9a3012d4b98c188
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-09-24 00:26:01 -0400

    Time: 44 ms (95.02%), Space: 22.2 MB (44.22%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,44 +8,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # store ancestors in each node?
-        # can early return when both nodes visited
-        root.par = None
+        if not root or root.val == p.val or root.val == q.val:
+            return root
         
-        def dfs(node):
-            # store direct parent in each node
-            if node.left:
-                node.left.par = node
-                dfs(node.left)
-            if node.right:
-                node.right.par = node
-                dfs(node.right)
-        
-        dfs(root)
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
-        # now p and q should have parents
-        # travel up parents to generate list of ancestors
-        p_ances = []
-        curr = p
-        while curr:
-            p_ances.append(curr)
-            curr = curr.par
+        # targets found in both sub trees
+        if left and right:
+            return root
         
-        q_ances = []
-        curr = q
-        while curr:
-            q_ances.append(curr)
-            curr = curr.par
-        
-        # find rightmost parent
-        # since its reversed, we find leftmost common
-        if len(p_ances) < len(q_ances):
-            # pick shorter ances chain to go up
-            small, big = p_ances, q_ances
-        else:
-            small, big = q_ances, p_ances
-        
-        big = set(big)
-        for node in small:
-            if node in big:
-                return node
+        return left or right
