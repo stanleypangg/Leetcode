41b799107c47b3472a299d17014335b6ac0376d1
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-09-23 21:40:59 -0400

    Time: 5504 ms (17.4%), Space: 21.4 MB (94.65%) - LeetHub

--- 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.py
+++ 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.py
@@ -1,20 +1,18 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         k_count = nums.count(k)
-        max_count = 0
+        max_gain = 0
         for i in range(1, 51):
             if i == k:
                 continue
-            curr_count = 0
-            curr_max = 0
+            curr_count = curr_max = 0
             for num in nums:
                 if num == i:
                     curr_count += 1
-                if num == k:
+                elif num == k:
                     curr_count -= 1
-                if curr_count < 0:
-                    curr_count = 0
+                curr_count = max(curr_count, 0)
                 curr_max = max(curr_max, curr_count)
-            max_count = max(max_count, curr_max)
-        return k_count + max_count
+            max_gain = max(max_gain, curr_max)
+        return k_count + max_gain
                 
