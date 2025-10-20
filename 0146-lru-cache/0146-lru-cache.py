f3bb67930d094cee73bfcfcee3cba6a274c2ea72
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-10-19 21:13:15 -0400

    Time: 167 ms (25.01%), Space: 78.5 MB (15.43%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,48 +1,78 @@
-class Node():
-    def __init__(self, key, val):
-        self.key = key
+class Node:
+    def __init__(self, val, prev = None, next = None, cached = False):
         self.val = val
-        self.prev = None
-        self.next = None
+        self.prev = prev
+        self.next = next
+        self.cached = cached
+
+    def remove(self):
+        # Remove self from linked list
+        temp = self.prev
+        self.prev.next = self.next
+        self.next.prev = temp
+        self.cached = False
+    
+    def insert(self, node):
+        # insert node after self
+        temp = self.next
+        self.next = node
+        node.prev, node.next = self, temp
+        temp.prev = node 
+        node.cached = True
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
-        self.lru = Node(0, 0)
-        self.mru = Node(0, 0)
-        self.lru.next = self.mru
-        self.mru.prev = self.lru
-    
-    def remove_node(self, node):
-        prev, next = node.prev, node.next
-        prev.next = next
-        next.prev = prev
-    
-    def add_node(self, node):
-        prev, next = self.mru.prev, self.mru
-        prev.next = next.prev = node
-        node.prev = prev
-        node.next = next
+
+        # hashmap: key -> node
+        # node has prev, next, val, isCached
+        # prev and next to do removal in O(1)
+        self.hashmap = {}
+
+        # initialize dummy nodes
+        self.head = Node(0) # head
+        self.tail = Node(0) # tail
+        self.head.next, self.tail.prev = self.tail, self.head
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove_node(self.cache[key])
-            self.add_node(self.cache[key])
-            return self.cache[key].val
+        # O(1): use a hashmap
+        # get: get the key, if exists, check if its within cache
+        # check within cache: keep a boolean
+
+        if key in self.hashmap and self.hashmap[key].cached:
+            # Enforce LRU
+            # Remove Node, and insert at tail
+            node = self.hashmap[key]
+            node.remove()
+            self.tail.prev.insert(node)
+
+            return node.val
+        
         return -1
 
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove_node(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.add_node(self.cache[key])
 
-        if len(self.cache) > self.capacity:
-            lru = self.lru.next
-            self.remove_node(lru)
-            del self.cache[lru.key]
+    def put(self, key: int, value: int) -> None:
+        # O(1): use a hashmap to put
+        # use a linked list to enforce LRU policy
+        # evict LRU is self.capacity == 0
+        
+        if self.get(key) != -1:
+            # Key exists, self.get handles LRU policy
+            node = self.hashmap[key]
+            node.val = value
+            return
+
+        # Otherwise, we insert at end, and handle it ourselves
+        if self.capacity == 0:
+            # Not enough space, we need to evict
+            self.head.next.remove()
+            self.capacity += 1
+        
+        new_node = Node(value)
+        self.hashmap[key] = new_node # create new node
+        self.tail.prev.insert(new_node)
+        self.capacity -= 1
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
