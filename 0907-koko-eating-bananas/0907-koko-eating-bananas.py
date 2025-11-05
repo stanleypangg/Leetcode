15387ef76fba58462f2b9330a6824a0443448c10
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-11-05 17:04:14 -0500

    Time: 163 ms (42.51%), Space: 19.3 MB (21%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,17 +1,33 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        k = r
+        # n = len(piles)
+        # h hours
+        # k bananas per hour
+        
+        # each hr: choose some pile and eat k bananas from that pile
+        # if less than k bananas, eats all instead and idles
 
+        # return minimum integer k s.t. she can eat all bananads within h hours
+
+        # binary search to find minimum integer k
+        
+        l = 1
+        r = k = max(piles)
+
+        # (n log n)
         while l <= r:
             mid = (l + r) // 2
-            # Use ceil
-            time = sum(math.ceil(i / mid) for i in piles)
 
-            if time <= h:
-                k = min(k, mid)
+            time = h
+            for pile in piles:
+                time -= math.ceil(pile / mid)
+                if time < 0:
+                    break
+            
+            if time >= 0:
+                k = mid
                 r = mid - 1
             else:
                 l = mid + 1
-        
+
         return k
