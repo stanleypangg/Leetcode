3e5dca17db6d4770114ad195ecffc5d2171a4779
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-11-24 19:39:16 -0500

    Time: 19 ms (37.05%), Space: 22.7 MB (24.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,9 +3,13 @@ class Solution:
         hashmap = {}
 
         for string in strs:
-            sorted_string = ''.join(sorted(string))
-            if sorted_string not in hashmap:
-                hashmap[sorted_string] = []
-            hashmap[sorted_string].append(string)
+            counter = [0] * 26
+            for c in string:
+                counter[ord(c) - ord('a')] += 1
+            
+            key = tuple(counter)
+            if key not in hashmap:
+                hashmap[key] = []
+            hashmap[key].append(string)
         
         return list(hashmap.values())
