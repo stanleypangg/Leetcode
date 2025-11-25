02926b393838de0ffbc090ff7d1e3dba611434de
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-11-24 19:36:15 -0500

    Time: 6 ms (98.61%), Space: 20.7 MB (61.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_map = {}
+        hashmap = {}
 
-        for s in strs:
-            key = "".join(sorted(s))
-            if key not in anagram_map:
-                anagram_map[key] = []
-            anagram_map[key].append(s)
-
-        return list(anagram_map.values())
+        for string in strs:
+            sorted_string = ''.join(sorted(string))
+            if sorted_string not in hashmap:
+                hashmap[sorted_string] = []
+            hashmap[sorted_string].append(string)
+        
+        return list(hashmap.values())
