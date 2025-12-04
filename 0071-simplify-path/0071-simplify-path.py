f1ed9ef5d51507f886611a372b8535cd721d4304
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-12-03 22:42:04 -0500

    Time: 0 ms (100%), Space: 17.6 MB (96.97%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -2,30 +2,12 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
 
-        i = 0
-        while i < len(path):
-            curr = path[i]
-
-            # Clear any slashes
-            while i < len(path) and path[i] == '/':
-                i += 1
-            
-            if i == len(path):
-                break
-
-            if i < len(path) and path[i] == '.' and (i == len(path) - 1 or path[i + 1] == '/'):
-                # Current diectory
-                i += 2
-            elif i < len(path) - 1 and path[i] == path[i + 1] == '.' and (i == len(path) - 2 or path[i + 2] == '/'):
-                # Previous directory
-                if stack:
-                    stack.pop()
-                i += 3
-            else:
-                l = r = i
-                while r < len(path) and path[r] != '/':
-                    r += 1
-                stack.append(path[l: r])            
-                i = r
+        for c in path.split('/'):
+            if c:
+                if c == '..':
+                    if stack:
+                        stack.pop()
+                elif c != '.':
+                    stack.append(c)
         
         return '/' + '/'.join(stack)
