9abe0df2ab34bf10931d869c332a6b73d9239345
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-12-07 23:48:44 -0500

    Time: 0 ms (100%), Space: 8.4 MB (12.62%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int hammingWeight(int n) {
         int res = 0;
-
-        for (int i = 0; i < 32; ++i)
-        {
-            if ((n >> i) & 1)
+        while (n != 0) {
+            if ((n & 1) == 1)
                 ++res;
+            n >>= 1;
         }
-
-        return res;;;
+        return res;
     }
 };
