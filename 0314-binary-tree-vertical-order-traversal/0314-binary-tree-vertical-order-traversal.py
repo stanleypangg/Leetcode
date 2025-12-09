7f695f79e345392d0b4a8e6016306b652da0432d
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-12-09 01:56:15 -0500

    Time: 1 ms (26.9%), Space: 17.7 MB (91.19%) - LeetHub

---
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # traverse entire tree for O(n) time
+        # how to output sorted list?
+        if not root:
+            return []
+
+        leftmost = rightmost = 0
+        res = defaultdict(list)
+
+        q = deque([(root, 0)])
+        while q:
+            for _ in range(len(q)):
+                node, x_pos = q.popleft()
+
+                res[x_pos].append(node.val)
+
+                leftmost = min(leftmost, x_pos)
+                rightmost = max(rightmost, x_pos)
+                
+                if node.left:
+                    q.append((node.left, x_pos - 1))
+                if node.right:
+                    q.append((node.right, x_pos + 1))
+        
+        return [res[i] for i in range(leftmost, rightmost + 1)]
