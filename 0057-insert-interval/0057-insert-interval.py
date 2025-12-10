f5936c7eb1544d39bf72e873b3fd24d2921898bc
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-12-10 16:36:13 -0500

    Time: 4 ms (19.49%), Space: 19.8 MB (18.34%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,34 +1,18 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
         res = []
-        n_start, n_end = newInterval
 
-        def insertMerge(start, end):
-            # just insert
-            if not res:
-                res.append([start, end])
-                return
-
-            prev_start, prev_end = res[-1]
-
-            if max(start, prev_start) <= min(end, prev_end):
-                res[-1][0] = min(start, prev_start)
-                res[-1][1] = max(prev_end, end)
-            else:
-                res.append([start, end])
+        i = 0
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
         
-        for start, end in intervals:
-            if res:
-                p_start, p_end = res[-1]
-            else:
-                p_start = p_end = -1
-            
-            if p_start <= n_start <= end:
-                insertMerge(n_start, n_end)
-            
-            insertMerge(start, end)
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i += 1
         
-        if not intervals or n_start > intervals[-1][1]:
-            insertMerge(n_start, n_end)
-
+        res.append(newInterval)
+        res.extend(intervals[i:])
         return res
