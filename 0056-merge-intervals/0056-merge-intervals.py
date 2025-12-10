3cb159e6d08786f785982ec0cf43209e928cfcf5
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-12-10 16:44:25 -0500

    Time: 7 ms (73.35%), Space: 21.8 MB (16.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,15 +1,13 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # O(nlogn): sort intervals, then just find overlap
-        res = []
         intervals.sort()
-        
-        for interval in intervals:
-            # if res is empty or most recent end doesn't overlap next start
-            if not res or res[-1][1] < interval[0]:
-                res.append(interval)
+        res = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            start, end = intervals[i]
+            if start <= res[-1][1]:
+                res[-1][1] = max(end, res[-1][1])
             else:
-                # overlap, externd end of furthest one
-                res[-1][1] = max(res[-1][1], interval[1])
+                res.append([start, end])
         
         return res
