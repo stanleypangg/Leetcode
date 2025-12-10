c046ec2fcb9df98a52074337bf1b5a19f272628f
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-12-10 18:31:30 -0500

    Time: 4 ms (37.86%), Space: 18.6 MB (36.94%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,28 +3,36 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # loop over every element
-        # if 0 encountered, we can just mark its rows and cols to placeholder
-        # if another 0 encoutnered in this process, we can just ignore it, since we 
-        # need to differentiate between a new zero and an original zero
-        # once finished, pass over the entire matrix again and change all the placeholders to zero
+        # O(mn), keey arrays rows and cols, to track which rows and cols need to be zero'd out
+        # this is O(mn) space however
+        # instead, store this data int he first row and col of the matrix
+        # iterate through entire matrix, and mark first row/col
+        # this is ok, since the first row/col value would be zero'd out anyways
+        # need to process first col/row last to not overwrite information
+        # use flags to mark if they are to be zero'd out
 
+        first_col_zeroed = first_row_zeroed = False
         m, n = len(matrix), len(matrix[0])
-        
         for r in range(m):
             for c in range(n):
                 if matrix[r][c] == 0:
-                    for dr in range(m):
-                        if matrix[dr][c] == 0:
-                            continue
-                        matrix[dr][c] = '*'
-                    for dc in range(n):
-                        if matrix[r][dc] == 0:
-                            continue
-                        matrix[r][dc] = '*'
-                    matrix[r][c] = '*'
+                    matrix[0][c] = 0
+                    matrix[r][0] = 0
+                    first_row_zeroed |= r == 0
+                    first_col_zeroed |= c == 0
         
-        for r in range(m):
-            for c in range(n):
-                if matrix[r][c] == '*':
-                    matrix[r][c] = 0
+        for r in range(1, m):
+            if matrix[r][0] == 0:
+                matrix[r] = [0] * n
+            
+        for c in range(1, n):
+            if matrix[0][c] == 0:
+                for dr in range(m):
+                    matrix[dr][c] = 0
+
+        if first_row_zeroed:
+            matrix[0] = [0] * n
+        
+        if first_col_zeroed:
+            for dr in range(m):
+                matrix[dr][0] = 0
