cdd0c8cd763e97f8cc1b14dc14f83a143dd91a60
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-12-15 12:32:02 -0500

    Time: 1 ms (50.06%), Space: 18.2 MB (20.57%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,25 +1,12 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # for each 0 <= i < num1.length find j s.t. nums1[i] == nums[j]
-        # and detrermine the next greater element in nums2[j], if none, the ans is -1
-
-        # O(n) time and space, n = nums1.length
-        ans = [-1] * len(nums1)
-
-        # O(n) time and space
-        hashmap = {}
-        for i, n in enumerate(nums1):
-            hashmap[n] = i
-        
-        # O(m) time and space, m = nums2.length
+        ans = {}
         st = []
-        for i, n in enumerate(nums2):
-            while st and n > st[-1]:
+
+        for num in nums2:
+            while st and num > st[-1]:
                 top = st.pop()
-                if top in hashmap:
-                    index = hashmap[top]
-                    ans[index] = n
-            
-            st.append(n)
+                ans[top] = num
+            st.append(num)
         
-        return ans
+        return [ans.get(num, -1) for num in nums1]
