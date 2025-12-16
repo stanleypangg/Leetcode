8e1dc9ecbfb83097b2f76fd072f2726b4744f0c8
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-12-16 10:47:33 -0500

    Time: 7 ms (75.74%), Space: 18.1 MB (19.8%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,22 +1,24 @@
 class Solution:
-    def rle_encode(self, string: str) -> str:
-        encoding = []
-
-        i = 0
-        while i < len(string):
-            j = i + 1
-            while j < len(string) and string[j] == string[j - 1]:
-                j += 1
-            encoding.append(str(j - i))
-            encoding.append(string[i])
-            i = j
-        
-        return ''.join(encoding)
-
     def countAndSay(self, n: int) -> str:
-        rle = '1'
+        def rle_encode(string: str) -> str:
+            encoding = []
 
+            i = 0
+            while i < len(string):
+                j = i + 1
+                while j < len(string) and string[j] == string[j - 1]:
+                    j += 1
+                encoding.append(str(j - i))
+                encoding.append(string[i])
+                i = j
+            
+            return ''.join(encoding)
+
+        if n == 1:
+            return '1'
+
+        rle = '1'
         for _ in range(n - 1):
-            rle = self.rle_encode(rle)
+            rle = rle_encode(rle)
         
         return rle
