a3c520fb402cac8bd92ffbab52f67afad37e45e2
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-12-17 12:06:59 -0500

    Time: 15 ms (89.04%), Space: 23.3 MB (43.96%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -4,11 +4,11 @@ class Solution:
             return -1
         
         res = None
-        diff = [gas[i] - cost[i] for i in range(len(gas))]
         max_postfix = float('-inf')
         postfix = 0
+        
         for i in range(len(gas) - 1, -1, -1):
-            postfix += diff[i]
+            postfix += gas[i] - cost[i]
             if postfix > max_postfix:
                 max_postfix = postfix
                 res = i
