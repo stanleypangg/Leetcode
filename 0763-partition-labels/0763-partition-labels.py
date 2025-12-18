5664d7c03e0edd83c77a57a1daf496686ff50376
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-12-18 11:56:59 -0500

    Time: 3 ms (92.01%), Space: 17.5 MB (99.87%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,31 +1,16 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        # greedy
-        # get the leftmost and rightmost indices of each char
-        # try to grab the smallest range, see which other chars are in there
-        # and make sure their ranges fit, if not, we have to extend the range
-
         res = []
-        last = {c : s.rindex(c) for c in set(s)}
+        last = {}
+        for i, c in enumerate(s):
+            last[c] = i
 
-        seen = set()
-        partition = set()
-        l = r = 0
-        while r < len(s):
-            # first seen in this partition
-            if s[r] not in seen:
-                seen.add(s[r])
-                partition.add(s[r])
-            
-            # if its the last index
-            if r == last[s[r]]:
-                partition.remove(s[r])
+        size = end = 0
+        for i, c in enumerate(s):
+            size += 1
+            end = max(end, last[c])
 
-            if not partition:
-                res.append(r - l + 1)
-                seen = set()
-                partition = set()
-                l = r + 1
-
-            r += 1
+            if i == end:
+                res.append(size)
+                size = 0
         return res
