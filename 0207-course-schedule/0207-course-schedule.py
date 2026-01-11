4f040640b8938ae3f3112e46fa26a69f71017a52
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-01-10 22:49:04 -0500

    Time: 8 ms (22.05%), Space: 21.8 MB (5.12%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,29 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        self.visited = set()
-        self.pre_map = defaultdict(set)
-
-        for course, pre in prerequisites:
-            self.pre_map[course].add(pre)
-
-        def dfs(course):
-            if course in self.visited:
+        # pre - crs
+        adj = defaultdict(set)
+        for crs, pre in prerequisites:
+            adj[crs].add(pre)
+        
+        visited = set()
+        def dfs(crs):
+            if crs in visited:
+                # cycle!
                 return False
-
-            pre_set = self.pre_map[course]
-            if not pre_set:
-                return True
-
-            self.visited.add(course)
-            for pre in pre_set:
+        
+            visited.add(crs)
+            for pre in adj[crs]:
                 if not dfs(pre):
                     return False
-            self.visited.remove(course)
-            self.pre_map[course] = []
+            visited.remove(crs)
+            adj[crs] = []
 
             return True
-        
-        for course in range(numCourses):
-            if not dfs(course):
+
+        for crs in range(numCourses):
+            if not dfs(crs):
                 return False
+        
         return True
