f3711cb9cc0e4bd5d902d25489c22f3aebbe0fab
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-01-10 23:14:41 -0500

    Time: 3 ms (75.43%), Space: 20.4 MB (5.88%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,34 +1,26 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = [[] for _ in range(numCourses)]
+        res = []
+
+        indeg = [0] * numCourses
+        adj = defaultdict(list)
         for crs, pre in prerequisites:
             adj[pre].append(crs)
+            indeg[crs] += 1
         
-        res = [0] * numCourses
-        self.i = numCourses - 1
-
-        # 0 = unvisited, 1 = visiting, 2 = visited
-        state = [0] * numCourses
-
-        def dfs(crs):
-            if state[crs] == 1:
-                return False
-            if state[crs] == 2:
-                return True
-            
-            state[crs] = 1
-            for neighbour in adj[crs]:
-                if not dfs(neighbour):
-                    return False
-            state[crs] = 2
-            res[self.i] = crs
-            self.i -= 1
-        
-            return True
-        
+        q = deque([])
         for i in range(numCourses):
-            if state[i] == 0:
-                if not dfs(i):
-                    return []
+            if indeg[i] == 0:
+                q.append(i)
+
+        while q:
+            u = q.popleft()
+            res.append(u)
+            for v in adj[u]:
+                indeg[v] -= 1
+                if indeg[v] == 0:
+                    q.append(v)
         
+        if len(res) != numCourses:
+            return []
         return res
