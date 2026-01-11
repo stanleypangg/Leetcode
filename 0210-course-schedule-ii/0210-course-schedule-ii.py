59b90ee9b512d71dbb7e47b8f125c1de2f87ead4
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-01-11 17:35:14 -0500

    Time: 3 ms (75.39%), Space: 20.4 MB (8.55%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,30 +1,25 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        res = []
-
+        indeg = [0] * numCourses
         adj = defaultdict(list)
         for crs, pre in prerequisites:
-            adj[crs].append(pre)
+            adj[pre].append(crs)
+            indeg[crs] += 1
 
-        # 0 = unvisited, 1 = visiting, 2 = visited
-        state = [0] * numCourses
-        def dfs(crs):
-            if state[crs] == 1:
-                return False # cycle!
-            if state[crs] == 2:
-                return True # we are already done with this
-            
-            # set current to visiting
-            state[crs] = 1
-            for pre in adj[crs]:
-                if not dfs(pre):
-                    return False
-            state[crs] = 2
-            res.append(crs)
-
-            return True
-
+        q = deque([])
         for crs in range(numCourses):
-            if not dfs(crs):
-                return []
+            if indeg[crs] == 0:
+                q.append(crs)
+        
+        res = []
+        while q:
+            u = q.popleft()
+            res.append(u)
+            for v in adj[u]:
+                indeg[v] -= 1
+                if indeg[v] == 0:
+                    q.append(v)
+        
+        if len(res) != numCourses:
+            return []
         return res
