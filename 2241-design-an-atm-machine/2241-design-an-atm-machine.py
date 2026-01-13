b39742c062c2c359af481f9ee3fa832d1adc0807
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-01-13 02:06:23 -0500

    Time: 24 ms (73.2%), Space: 21.8 MB (5.41%) - LeetHub

--- 2241-design-an-atm-machine/2241-design-an-atm-machine.py
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.py
@@ -1,33 +1,33 @@
 class ATM:
     # denominations: 20, 50, 100, 200, 500
+    DENOMS = (20, 50, 100, 200, 500)
 
     def __init__(self):
-        # assume reversed order for both
-        self.denoms = (20, 50, 100, 200, 500)
-        self.n = len(self.denoms)
-        self.money = [0] * self.n
+        self.money = [0] * len(self.DENOMS)
 
     def deposit(self, banknotesCount: List[int]) -> None:
-        for i, m in enumerate(banknotesCount):
-            self.money[i] += m
+        for i, count in enumerate(banknotesCount):
+            self.money[i] += count
 
     def withdraw(self, amount: int) -> List[int]:
-        res = [0] * self.n
+        res = [0] * len(self.DENOMS)
+        remaining = amount
 
-        for i in range(self.n - 1, -1, -1):
-            denom = self.denoms[i]
-            notes = min(self.money[i], amount // denom)
-            res[i] = notes
-            
-            amount -= notes * denom
-            if amount == 0:
+        for i in range(len(self.DENOMS) - 1, -1, -1):
+            denom = self.DENOMS[i]
+            take = min(self.money[i], remaining // denom)
+            res[i] = take
+            remaining -= take * denom
+
+            if remaining == 0:
                 break
         
-        if amount > 0:
+        if remaining != 0:
             return [-1]
 
-        for i, count in enumerate(res):
-            self.money[i] -= count
+        for i in range(len(self.DENOMS)):
+            self.money[i] -= res[i]
+            
         return res
 
 # Your ATM object will be instantiated and called as such:
