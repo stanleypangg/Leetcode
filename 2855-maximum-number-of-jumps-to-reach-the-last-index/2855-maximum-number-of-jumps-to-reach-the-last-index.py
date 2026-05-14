ab2ea3c74c1bd4a559434aa71fac2032f8eefe4a
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-05-14 14:48:57 -0400

    2855-MAXIMUM-NUMBER-OF-JUMPS-TO-REACH-THE-LAST-INDEX Stats: Time: 319 ms (95.54%), Space: 19.5 MB (61.82%) - LeetHub

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/2855-maximum-number-of-jumps-to-reach-the-last-index.py
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/2855-maximum-number-of-jumps-to-reach-the-last-index.py
@@ -1,16 +1,17 @@
 class Solution:
     def maximumJumps(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        dp = [0] * n
+        dp = [-1] * n
+        dp[0] = 0
 
         for i in range(n):
             # first check if its even reached
-            if i > 0 and dp[i] == 0:
+            if dp[i] == -1:
                 continue
-                
+
             for j in range(i + 1, n):
                 # check if you can jump there
                 if abs(nums[j] - nums[i]) <= target:
                     dp[j] = max(dp[j], dp[i] + 1)
                 
-        return -1 if dp[n - 1] == 0 else dp[n - 1]
+        return -1 if dp[n - 1] == -1 else dp[n - 1]
