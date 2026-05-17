2124f05237c1523e124402c3e5e38787e98066b5
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-05-17 01:14:16 -0400

    2203-NUMBER-OF-SPACES-CLEANING-ROBOT-CLEANED Stats: Time: 0 ms (100%), Space: 23.7 MB (88.94%) - LeetHub

--- 2203-number-of-spaces-cleaning-robot-cleaned/2203-number-of-spaces-cleaning-robot-cleaned.py
+++ 2203-number-of-spaces-cleaning-robot-cleaned/2203-number-of-spaces-cleaning-robot-cleaned.py
@@ -1,37 +1,28 @@
 class Solution:
     def numberOfCleanRooms(self, room: List[List[int]]) -> int:
-        res = 0
         m, n = len(room), len(room[0])
         dirs = ((0, 1), (1, 0), (0, -1), (-1, 0)) # RDLU
         rooms = set()
         states = set()
 
-        cur = (0, 0, 0)
-        while cur not in states:
-            states.add(cur)
-            r, c, direction = cur
-
-            if (r, c) not in rooms:
-                res += 1
-                rooms.add((r, c))
+        r = c = direction = 0
+        while (r, c, direction) not in states:
+            states.add((r, c, direction))
+            rooms.add((r, c))
 
             rotations = 0
 
             for _ in range(4):
                 dr, dc = dirs[direction]
-                nr = r + dr
-                nc = c + dc
+                nr, nc = r + dr, c + dc
 
                 # now check if valid
                 if nr >= 0 and nr < m and nc >= 0 and nc < n and room[nr][nc] == 0:
+                    r, c = nr, nc
                     break
 
                 direction = (direction + 1) % 4
-                rotations += 1
-        
-            if rotations == 4:
-                return res
+            else:
+                break
             
-            cur = (nr, nc, direction)
-
-        return res
+        return len(rooms)
