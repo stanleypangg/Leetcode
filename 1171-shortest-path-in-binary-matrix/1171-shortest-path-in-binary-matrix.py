00e9c692f225d910d7bf693d279f4ac87773783c
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-05-21 15:03:24 -0400

    1171-SHORTEST-PATH-IN-BINARY-MATRIX Stats: Time: 203 ms (28.15%), Space: 20.8 MB (13.66%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,37 +1,35 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
+        n = len(grid)
         
-        if grid[0][0] == 1:
+        if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
             return -1
-        if m == 1 and n == 1:
+        if n == 1:
             return 1
         
 
         dirs = ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1))
-        best = {}
-        best[(0, 0)] = 1
+        visited = set()
+        visited.add((0, 0))
         q = deque([(0, 0, 1)])
 
         while q:
             r, c, length = q.popleft()
-            if r == m - 1 and c == n - 1:
+            if r == n - 1 and c == n - 1:
                 return length
             
             for dr, dc in dirs:
                 nr, nc = r + dr, c + dc
                 n_length = length + 1
                 
-                out_of_bounds = nr < 0 or nr >= m or nc < 0 or nc >= n
+                out_of_bounds = nr < 0 or nr >= n or nc < 0 or nc >= n
                 if out_of_bounds:
                     continue
                 
-                blocked = grid[nr][nc] == 1
-                not_better = n_length >= best.get((nr, nc), float('inf'))
-                if blocked or not_better:
+                if grid[nr][nc] == 1 or (nr, nc) in visited:
                     continue
                 
                 q.append((nr, nc, n_length))
-                best[(nr, nc)] = n_length
+                visited.add((nr, nc))
         
         return -1
