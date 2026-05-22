959ae7d269ff870725691b97818323185e41df6f
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-05-22 02:42:35 -0400

    0006-ZIGZAG-CONVERSION Stats: Time: 8 ms (71.12%), Space: 19.4 MB (47.43%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,19 +2,17 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-            
-        rows = [[] for _ in range(numRows)]
+
+        rows = [''] * numRows
         idx = 0
         phase = 1
 
         for c in s:
-            rows[idx].append(c)
-
+            rows[idx] += c
             if idx == 0:
                 phase = 1
             elif idx == numRows - 1:
                 phase = -1
-            
             idx += phase
         
-        return ''.join(''.join(row) for row in rows)
+        return ''.join(rows)
