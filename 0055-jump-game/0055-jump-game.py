a70dfdf0ab13c5ffe84b1f8ac874698e70babbec
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-05-24 22:31:33 -0400

    0055-JUMP-GAME Stats: Time: 23 ms (46.37%), Space: 20.2 MB (65.24%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # just need to make it past all 0s
-        jump = 0
-        for pos, num in enumerate(nums):
-            jump = max(jump, pos + num)
-            if jump >= len(nums) - 1:
-                return True
-            elif jump == pos:
+        farthest = 0
+
+        for i, jump in enumerate(nums):
+            if i > farthest:
                 return False
+
+            farthest = max(farthest, i + jump)
         
+        return True
