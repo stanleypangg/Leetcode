78f29844713fc2c03de71d9715991947a4bec353
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-06-17 12:42:02 -0400

    0547-NUMBER-OF-PROVINCES Stats: Time: 6 ms (53.66%), Space: 21.4 MB (37.15%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,34 +1,18 @@
-class UnionFind:
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.size = [1] * n
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, a, b):
-        rootA = self.find(a)
-        rootB = self.find(b)
-
-        if rootA == rootB:
-            return
-        
-        if self.size[rootA] < self.size[rootB]:
-            rootA, rootB = rootB, rootA
-        
-        self.parent[rootB] = rootA
-        self.size[rootA] += self.size[rootB]
-
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        uf = UnionFind(n)
+        res = 0
+        visited = [False] * n
 
-        for i in range(n):
+        def dfs(i):
+            visited[i] = True
             for j in range(n):
-                if isConnected[i][j]:
-                    uf.union(i, j)
+                if isConnected[i][j] and not visited[j]:
+                    dfs(j)
+            
+        for i in range(n):
+            if not visited[i]:
+                res += 1
+                dfs(i)
         
-        return len({uf.find(i) for i in range(n)})
+        return res
