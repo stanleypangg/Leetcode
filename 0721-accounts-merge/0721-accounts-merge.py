9a61a0b0873b3fb264d634f925c2f177c0930a50
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-06-19 11:06:51 -0400

    0721-ACCOUNTS-MERGE Stats: Time: 8 ms (99.16%), Space: 22.2 MB (66.77%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,35 +1,27 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        adj = defaultdict(list)
-        email_to_name = {}
-        for account in accounts:
-            name = account[0]
-            for i in range(1, len(account)):
-                email1 = account[i]
-                email_to_name[email1] = name
+        parent = [i for i in range(len(accounts))]
 
-                for j in range(i + 1, len(account)):
-                    email2 = account[j]
-                    email_to_name[email2] = name
-                    adj[email1].append(email2)
-                    adj[email2].append(email1)
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
         
-        visited = set()
-        def dfs(cur, account):
-            visited.add(cur)
-            account.append(cur)
-            
-            for nei in adj[cur]:
-                if nei not in visited:
-                    dfs(nei, account)
+        def union(x, y):
+            parent[find(x)] = find(y)
         
-        res = []
-        for email in email_to_name:
-            if email not in visited:
-                name = email_to_name[email]
-                account = [name]
-                dfs(email, account)
-                account[1:] = sorted(account[1:])
-                res.append(account)
+        email_to_account = {}
+
+        for i, account in enumerate(accounts):
+            for email in account[1:]:
+                if email in email_to_account:
+                    union(i, email_to_account[email])
+                else:
+                    email_to_account[email] = i
         
-        return res
+        groups = defaultdict(list)
+        for email, i in email_to_account.items():
+            root = find(i)
+            groups[root].append(email)
+        
+        return [[accounts[i][0]] + sorted(emails) for i, emails in groups.items()]
