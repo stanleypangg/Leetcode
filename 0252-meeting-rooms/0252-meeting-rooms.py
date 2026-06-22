ca9674c4b99c5c0156117a343c194c892a90b35a
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-06-22 00:57:35 -0400

    0252-MEETING-ROOMS Stats: Time: 4 ms (27.74%), Space: 21.3 MB (40.63%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,7 +1,11 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if 0 <= len(intervals) <= 1:
+            return True
+
         intervals.sort()
-        for i in range(len(intervals) - 1):
-            if intervals[i][1] > intervals[i + 1][0]:
+        for i, (start, end) in enumerate(intervals[1:], start=1):
+            if start < intervals[i - 1][1]:
                 return False
+        
         return True
