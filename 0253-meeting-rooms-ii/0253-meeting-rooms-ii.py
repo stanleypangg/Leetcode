0fcfb89504719e81a43bc03cc2144da7a387a51c
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-06-22 01:01:31 -0400

    0253-MEETING-ROOMS-II Stats: Time: 4 ms (44.9%), Space: 21.3 MB (78.94%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,14 +1,20 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        res = 0
         intervals.sort()
         heap = []
-        res = 0
 
-        for s, e in intervals:
-            # while earliest ending room is <= current end
-            while heap and heap[0] <= s:
+        # since intervals is sorted, everything in heap has guarateed start <= cur_start
+        # min heap by end time
+
+        for start, end in intervals:
+            # while they don't overlap
+            # min heap grabs current largest end
+            while heap and heap[0] <= start:
                 heapq.heappop(heap)
-            heapq.heappush(heap, e)
+
+            # by here, if heap is non-empty, we have # of overlapping rooms
+            heapq.heappush(heap, end)
             res = max(res, len(heap))
 
         return res
