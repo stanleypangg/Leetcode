0611ee105698db56cf16c3e9d38c0f6191dd769e
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-06-27 21:07:05 -0400

    Time: 0 ms (100%), Space: 19.4 MB (56.76%) - LeetHub

--- 1958-check-if-move-is-legal/1958-check-if-move-is-legal.py
+++ 1958-check-if-move-is-legal/1958-check-if-move-is-legal.py
@@ -4,16 +4,16 @@ class Solution:
             return r >= 0 and r < 8 and c >= 0 and c < 8
 
         dirs = ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1))
-        opposite = {'B': 'W', 'W': 'B'}
+        opponent = 'W' if color == 'B' else 'B'
 
         for dr, dc in dirs:
             # check to see if direct next is oppposite
             nr, nc = rMove + dr, cMove + dc
-            if not in_bounds(nr, nc) or board[nr][nc] != opposite[color]:
+            if not in_bounds(nr, nc) or board[nr][nc] != opponent:
                 continue
             
             # now need to find next endpoint, which is same as color
-            while in_bounds(nr, nc) and board[nr][nc] == opposite[color]:
+            while in_bounds(nr, nc) and board[nr][nc] == opponent:
                 nr += dr
                 nc += dc
             
