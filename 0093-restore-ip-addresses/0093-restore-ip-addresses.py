53566ddac57989b50b49365f3041da4fa519cc3b
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-07-30 23:24:36 -0400

    Time: 1 ms (59.23%), Space: 19.3 MB (90.22%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -2,26 +2,22 @@ class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         res = []
 
-        cur = []
-        def bt(i):
-            if i >= len(s):
-                if len(cur) == 4:
-                    res.append('.'.join(cur))
+        def bt(i, path):
+            if len(path) == 4:
+                if i == len(s):
+                    res.append('.'.join(path))
                 return
             
-            cur.append(s[i])
-            bt(i + 1)
-            cur.pop()
+            for j in range(1, 4):
+                if i + j > len(s):
+                    break
 
-            if s[i] != '0':
-                if i + 1 < len(s):
-                    cur.append(s[i: i + 2])
-                    bt(i + 2)
-                    cur.pop()
-                if i + 2 < len(s) and int(s[i: i + 3]) <= 255:
-                    cur.append(s[i: i + 3])
-                    bt(i + 3)
-                    cur.pop()
+                part = s[i: i + j]
+
+                if (part[0] == '0' and j > 1) or int(part) > 255:
+                    continue
+                
+                bt(i + j, path + [part])
         
-        bt(0)
+        bt(0, [])
         return res
