1e77096f83fa547e0c1e5c1b2b52c52ef3fa574e
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-07-30 23:16:31 -0400

    Time: 103 ms (62.88%), Space: 20 MB (48.16%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        freq = Counter(word)
+        heap = [-v for v in freq.values()]
+        heapq.heapify(heap)
+
+        res = presses = 0
+        while heap:
+            cur = -heapq.heappop(heap)
+            res += cur * (presses // 8 + 1)
+            presses += 1
+        
+        return res
