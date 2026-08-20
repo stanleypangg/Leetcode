db348897323426cc3649160718403ed218d7ae4d
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-08-19 20:12:34 -0400

    Time: 31 ms (98.02%), Space: 20.4 MB (76.05%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,24 +1,35 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         word_set = set(wordList)
-        if endWord not in word_set:
+        if endWord not in wordList:
             return 0
 
-        q = deque()
-        q.append((beginWord, 1))
-        visited = {beginWord}
+        begin_set = {beginWord}
+        end_set = {endWord}
+        visited = set()
+        steps = 1
 
-        while q:
-            cur, length = q.popleft()
-            if cur == endWord:
-                return length
+        while begin_set and end_set:
+            # balance exploring on both ends
+            if len(begin_set) > len(end_set):
+                begin_set, end_set = end_set, begin_set
 
-            for i in range(len(cur)):
-                pre, post = cur[:i], cur[i+1:]
-                for char in 'abcdefghijklmnopqrstuvwxyz':
-                    next_word = pre + char + post
-                    if next_word in word_set and next_word not in visited:
-                        visited.add(next_word)
-                        q.append((next_word, length + 1))
-            
+            next_set = set()
+            for word in begin_set:
+                for i in range(len(word)):
+                    for char in string.ascii_lowercase:
+                        if char == word[i]:
+                            continue
+                        
+                        next_word = word[:i] + char + word[i+1:]
+                        if next_word in end_set:
+                            return steps + 1
+                        
+                        if next_word in word_set and next_word not in visited:
+                            next_set.add(next_word)
+                            visited.add(next_word)
+                
+            begin_set = next_set
+            steps += 1
+        
         return 0
