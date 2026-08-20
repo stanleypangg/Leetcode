d4e93270036868748d78e6290ed6e1fcdddc5d48
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-08-20 01:32:31 -0400

    Time: 7 ms (79.15%), Space: 19.5 MB (23.38%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.py
+++ 0265-paint-house-ii/0265-paint-house-ii.py
@@ -1,18 +1,22 @@
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
         n, k = len(costs), len(costs[0])
-        cur = costs[0]
+        prev = costs[0]
 
         for i in range(1, n):
+            min1, min2, min1_idx = float('inf'), float('inf'), -1
+            for j, cost in enumerate(prev):
+                if cost < min1:
+                    min1, min2, min1_idx = cost, min1, j
+                elif cost < min2:
+                    min2 = cost
+                
+            cur = [0] * k
+            for j in range(k):
+                prev_min = min1 if j != min1_idx else min2
+                cur[j] = prev_min + costs[i][j]
+            
             prev = cur
-            cur = [float('inf')] * k
-
-            for colour in range(k):
-                for other in range(k):
-                    if colour == other:
-                        continue
-                    
-                    cur[colour] = min(cur[colour], prev[other] + costs[i][colour])
         
-        return min(cur)
+        return min(prev)
                     
