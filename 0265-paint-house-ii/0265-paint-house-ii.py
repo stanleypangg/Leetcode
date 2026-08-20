9c6b628e9bc4e6d4992144dbe090c18d23345572
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-08-20 01:25:47 -0400

    Time: 39 ms (31.27%), Space: 19.4 MB (41.41%) - LeetHub

---
+++ 0265-paint-house-ii/0265-paint-house-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:
+        n, k = len(costs), len(costs[0])
+        cur = costs[0]
+
+        for i in range(1, len(costs)):
+            prev = cur
+            cur = [float('inf')] * k
+
+            for colour in range(k):
+                for other in range(k):
+                    if colour == other:
+                        continue
+                    
+                    cur[colour] = min(cur[colour], prev[other] + costs[i][colour])
+        
+        return min(cur)
+                    
